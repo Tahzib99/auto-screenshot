aaa1a830154daf48742c77b17493d13d11eb9d32
Tahzib99 <97920651+Tahzib99@users.noreply.github.com>
2022-03-31 09:20:59 -0500

    cleanup

--- src/screenshot.js
+++ src/screenshot.js
@@ -1,10 +1,9 @@
 const puppeteer = require('puppeteer');
 const devices = puppeteer.devices;
 const fs = require('fs');
-
 const {host, routes} = require('./config');
-let browser;
 
+let browser;
 
 const init = async () => {
   browser = await puppeteer.launch({ headless: false, timeout: 100000 });
@@ -52,33 +51,23 @@ async function takeScreenshot(options) {
   //changes the footer styles for full page screenshot
   // these are hacky ways to prevent the footer on trulicity from getting in the way
   // if you are not capturing the trulicity site, then remove the page.evaluate functions below
-  if(screenSize === 'Desktop'){
+  if(screenSize === 'Desktop') {
     await page.evaluate(() => {
       let footer = document.querySelector('.lds-isi');
       footer.style.width = "100%";
       footer.style.position = "relative";
       footer.style.top = null;
       // scroll down and up the page so footer content is displayed properly
-      window.scrollBy(0,window.document.body.scrollHeight);
+      window.scrollBy(0, window.document.body.scrollHeight);
       window.scrollBy(0, -window.document.body.scrollHeight);
     });
-  } else {
-    await page.evaluate(() => {
-      let footer = document.querySelector('.lds-isi');
-      footer.style.width = "100%";
-      footer.style.position = "relative";
-      footer.style.top = null;
-      // scroll down and up the page so footer content is displayed properly
-      window.scrollBy(0,window.document.body.scrollHeight);
-    });  
   }
-
   await page.screenshot({ path: `../screenshots/${filename}/${filename}_${screenSize}_Scroll.png`, fullPage: true });
 }
 
-const captureAllRoutes = async () => {
+const capturePages = async () => {
   // some sites require us to login before we can access them
-  // by setting this variable to true, on the first page request it will give us time to login
+  // by setting this variable to true, on the first page request it will give us time to login to the site
   let authorizationRequired = false;
   for (const [pageName, pageURL] of Object.entries(routes)) {
     await capturePage(pageName, pageURL, authorizationRequired);
@@ -92,7 +81,7 @@ const cleanup = () => {
 
 async function main() {
   await init();
-  await captureAllRoutes();
+  await capturePages();
   cleanup();
 }
 main();
